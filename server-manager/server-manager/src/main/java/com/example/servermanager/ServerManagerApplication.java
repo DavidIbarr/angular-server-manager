8ba4c182df07ba1a09649a6d4c023bb2ee3a1541
david.ibarra1452@gmail.com <77758167+DavidIbarr@users.noreply.github.com>
2023-01-31 22:15:57 -0600

    add bean to save servers for testing

--- server-manager/server-manager/src/main/java/com/example/servermanager/ServerManagerApplication.java
+++ server-manager/server-manager/src/main/java/com/example/servermanager/ServerManagerApplication.java
@@ -1,7 +1,14 @@
 package com.example.servermanager;
 
+import com.example.servermanager.model.Server;
+import com.example.servermanager.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.example.servermanager.enumeration.Status.SERVER_DOWN;
+import static com.example.servermanager.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerManagerApplication {
@@ -10,4 +17,13 @@ public class ServerManagerApplication {
 		SpringApplication.run(ServerManagerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower", "http://localhost:8080/server/image/server2.png", SERVER_DOWN));
+			serverRepository.save(new Server(null, "192.168.1.21", "MS 2008 Linux", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
+		};
+	}
 }
