171141974212e9e7bcc1100e5d347c78e27871fe
david.ibarra1452@gmail.com <77758167+DavidIbarr@users.noreply.github.com>
2023-02-12 16:59:51 -0600

    Called the Service to get data from the backend

--- server-manager/server-manager/src/main/frontend/server-frontend/src/app/app.component.ts
+++ server-manager/server-manager/src/main/frontend/server-frontend/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomResponse } from './models/interfaces/custom-response';
+import { AppState } from './models/enums/app-state';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { ServerService } from './service/server.service';
+import { DataState } from './models/enums/data-state.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'server-frontend';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+    .pipe(
+      map(response => {
+        return { 
+          dataState: DataState.LOADED_STATE, 
+          appData: response 
+        }
+      }),
+      startWith({
+          dataState: DataState.LOADING_STATE
+      }),
+      catchError((error: string) => {
+        return of({
+          dataState: DataState.ERROR_STATE, error
+        })
+      })
+    )
+
+    throw new Error('Method not implemented.');
+  }
+  
 }
